4a3b628df931e2cfa039c317f07f7bcba167abc7
Lenny132 <lcheng@fusebill.com>
2015-05-29 16:10:24 -0400

    Revert "Revert "Yet agains more missing files""
    This reverts commit 87e1cd4ddd9643ea73522f80fa2fe211209c4fb1.

---
+++ Fusebill.eCommerceWorkflow/Fusebill.eCommerceWorkflow/Controllers/PlansController.cs
@@ -0,0 +1,18 @@
+﻿using Fusebill.ApiWrapper;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace Fusebill.eCommerceWorkflow.Controllers
+{
+    public class PlansController : FusebillController
+    {
+        //
+        // GET: /Plans/
+        public ActionResult Index()
+        {
+            var result = ApiClient.GetPlans(null);
+            return View();
+        }
+
+    }
+}
---
+++ Fusebill.eCommerceWorkflow/Fusebill.eCommerceWorkflow/ReadMe.txt
@@ -0,0 +1,4 @@
+﻿The API wrapper library can be used at your own discretion.
+Update the AccountKey and the SecureUri values in the web config.
+Apply for an AccountKey at Fusebill.com
+
